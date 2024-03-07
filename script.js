d7adb008fda58e39c3ba4f155c4f0e049e56ef08
Eureka072002 <ankit2002rajkr@gmail.com>
2024-03-07 15:44:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+function reDirect(){
+    window.location.href = 'https://github.com/Eureka072002';
+}
+function reDirect1() {
+    location.href = 'https://www.linkedin.com/in/ankit-raj-03a908269/'
+}
+function reDirect2() {
+    location.href = 'https://www.linkedin.com/in/ankit-raj-03a908269/'
+}
+function reDirect3() {
+    location.href = 'https://www.linkedin.com/in/ankit-raj-03a908269/'
+}
